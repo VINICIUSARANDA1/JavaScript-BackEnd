41f2cef8215e1fd6d69fe5475261bc04954eb06e
VINICIUSARANDA1 <vinicius.aranda@outlook.com>
2024-01-29 11:55:27 -0300

    Continuando os estudos 29/01

---
+++ MediaComForEach.js
@@ -0,0 +1,5 @@
+/* Desafio: Calcular a média entre as seguintes notas usando o forEach  */
+
+const notas = [10, 6.5, 8, 7.5];
+
+notas.forEach()
---
+++ ProcuraListaAlunos.js
@@ -0,0 +1,21 @@
+/* Desafio: Procurar na lista de alunos, um aluno existente e exibir a nota dele  */
+
+
+const alunos = [ "João" , "Juliana", "Ana" , "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+function exibeNomeENota(aluno){
+    if(lista[0].includes(aluno)){
+    const indice = lista[0].indexOf(aluno);
+    const mediaAluno = lista[1][indice]
+    console.log(`${aluno} tem a média de ${mediaAluno}`)
+    }else{
+        console.log("Aluno não foi encontrado !")
+    }
+    
+};
+
+exibeNomeENota("Juliana");
+exibeNomeENota("Vini")
